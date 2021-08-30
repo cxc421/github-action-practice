21cdd287ea457d7424f13f8caf28b5b6bdda35d8
Chris Chuang <chrischuang0421@gmail.com>
2021-08-30 17:12:15 +0800

    Test failed action

--- dist/index.js
+++ dist/index.js
@@ -3262,10 +3262,17 @@ const axios = __nccwpck_require__(545);
 const core = __nccwpck_require__(186);
 
 const DEFAULT_CHARACTER = "amy";
+const AVAILABLE_CHARACTERS = new Set(["amy", "bender", "dr-zoidberg"]);
 
 async function run() {
   const character =
     core.getInput("character", { required: false }) || DEFAULT_CHARACTER;
+
+  if (!AVAILABLE_CHARACTERS.has(character)) {
+    core.setFailed(`Unknown character: ${character}`);
+    return;
+  }
+
   const response = await axios.get(
     `https://futuramaapi.herokuapp.com/api/characters/${character}/1`
   );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ const axios = require("axios");
 const core = require("@actions/core");
 
 const DEFAULT_CHARACTER = "amy";
+const AVAILABLE_CHARACTERS = new Set(["amy", "bender", "dr-zoidberg"]);
 
 async function run() {
   const character =
     core.getInput("character", { required: false }) || DEFAULT_CHARACTER;
+
+  if (!AVAILABLE_CHARACTERS.has(character)) {
+    core.setFailed(`Unknown character: ${character}`);
+    return;
+  }
+
   const response = await axios.get(
     `https://futuramaapi.herokuapp.com/api/characters/${character}/1`
   );
